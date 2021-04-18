83ee6361e0d4e88387e635f6422ca7f033635d87
unknown <lakshmehta06@gmail.com>
2021-04-18 21:48:51 +0530

    [Laksh added UC1]

---
+++ StackAndQueueUsingLinkedList/StackUsingLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueueUsingLinkedList
+{
+    class StackUsingLinkedList
+    {
+        private Node top;
+        public StackUsingLinkedList()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;//1300
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;//1400
+            }
+        }
+    }
+}
